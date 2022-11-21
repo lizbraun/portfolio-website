010d185b7b6e5b6562d85d97a25a201420474a41
lizbraun <elizabeth.braun@uconn.edu>
2022-11-21 16:54:42 -0500

    Update

--- js/main.js
+++ js/main.js
@@ -71,3 +71,17 @@ $(function() {
     });
   });
 
+
+  // test for making background stop scrolling when modal is open //
+document.body.style.position = 'fixed';
+document.body.style.top = `-${window.scrollY}px`;
+
+// When the modal is hidden, we want to remain at the top of the scroll position
+document.body.style.position = '';
+document.body.style.top = '';
+
+const scrollY = document.body.style.top;
+document.body.style.position = '';
+document.body.style.top = '';
+window.scrollTo(0, parseInt(scrollY || '0') * -1);
+
